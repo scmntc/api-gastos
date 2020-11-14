bc85c1d01f4b639a9ecdb407c848b6e0fb6f9038
Otavio Soares <tavinq@hotmail.com>
2020-11-14 12:05:49 -0300

    Initial Commit

---
+++ src/main/java/com/github/scmntc/apigastos/ApiGastosApplication.java
@@ -0,0 +1,13 @@
+package com.github.scmntc.apigastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGastosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGastosApplication.class, args);
+    }
+
+}
